c01cdd4e54ecf7ea6ccc053df94af918873f96ea
BrentBauman86 <brentbauman86@gmail.com>
2019-07-14 14:08:59 -0700

    begin building currentUser action and reducer

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,6 +1,6 @@
 export default (state = [], action) => {
     switch (action.type) {
-        case "ADD_USER":
+        case "CURRENT_USER":
             return action.user 
         default: 
             return state     
--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import usersReducer from './reducers/users'
 import thunk  from 'redux-thunk';
+import currentUser from './reducers/currentUser.js'
 
 const reducer = combineReducers({
-    user: usersReducer
+    user: usersReducer,
+    currentUser
 })
 
 const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
